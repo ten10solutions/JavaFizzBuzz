ce50ee28df69125f2add1d4761053a850db1bfcb
jack.forman <forman.jack@gmail.com>
2017-02-03 12:10:49 +0000

    removed code from main method

--- src/main/java/newFolder/App.java
+++ src/main/java/newFolder/App.java
@@ -13,23 +13,10 @@ import org.openqa.selenium.*;
 public class App {
 
 
-    /**
-     * This is a program which does xyz
-     * @param args
-     */
+
     public static void main (String args[]){
 
-//        Calendar rightNow = Calendar.getInstance();
-//        System.out.println(rightNow.getFirstDayOfWeek());
 
-
-        User player = new User();
-        Game newGame = new Game();
-
-        player.setName("Please enter your name");
-        player.setCounter("How high should I count - fizz buzz style");
-
-        newGame.playGame(player);
 
 
 
